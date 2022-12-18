c8f46a2754669679a5897e9fe9c71652a2c88f1b
Lionel Mason <capitalcookhouse@gmail.com>
2022-12-18 14:07:37 -0500

    commit tree 690954ec287a5b204b79134f66dbfa29a76d8cfc

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,40 @@
+class SpicesController < ApplicationController
+
+
+    #GET spices
+  def index
+    spice = Spice.all
+    render json: spice
+  end
+
+  #POST create a new spice
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  
+  end
+
+  #PATCH updates an existing spice
+  def update
+    spice = Spice.find_by(id: params[:id])
+    spice.update(spice_params)
+        render json: spice
+end
+
+
+  #DELETE an existing spice
+  def destroy 
+    spice = Spice.find_by(id: params[:id])
+    spice.destroy
+    head :no_content
+  end
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+
+end
+
